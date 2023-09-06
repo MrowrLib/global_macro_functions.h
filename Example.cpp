22d1092b92c4a47cdf0d495b6f87f0b158130e95
Mrowr Purr <mrowr.purr@gmail.com>
2023-09-05 19:06:48 -0700

    With args too

--- Example.cpp
+++ Example.cpp
@@ -7,13 +7,18 @@
 using namespace std;
 
 class StoresCoolBlocksOfCode {
-    vector<function<void()>> _codeBlocks;
+    vector<function<void()>>         _codeBlocks;
+    vector<function<void(int, int)>> _codeBlocksWithArguments;
 
 public:
     void AddCodeBlock(function<void()> codeBlock) { _codeBlocks.push_back(codeBlock); }
+    void AddCodeBlockWithArguments(function<void(int, int)> codeBlock) {
+        _codeBlocksWithArguments.push_back(codeBlock);
+    }
 
-    void ExecuteAllCodeBlocks() {
+    void ExecuteAllCodeBlocks(int arg1 = 69, int arg2 = 420) {
         for (auto codeBlock : _codeBlocks) codeBlock();
+        for (auto codeBlock : _codeBlocksWithArguments) codeBlock(arg1, arg2);
     }
 };
 
@@ -25,6 +30,17 @@ StoresCoolBlocksOfCode TheMainBlocksOfCode;
 // DSL blocks (with no arguments)
 CoolBlockOfCode { _Log_("Hello from FIRST CoolBlockOfCode!"); }
 CoolBlockOfCode { _Log_("Hello from SECOND CoolBlockOfCode!"); }
+
+#define BlockWithArguments                                                   \
+    _DSL_MAKER_REGISTER_NEW_FUNCTION_WITH_ARGUMENTS(                         \
+        TheMainBlocksOfCode.AddCodeBlockWithArguments, __COUNTER__, int, int \
+    )
+
+BlockWithArguments(int arg1, int arg2) {
+    _Log_("Hello from BlockWithArguments! arg1 = {}, arg2 = {}", arg1, arg2);
+}
+
+// NEXT: ones which have the (...) argument block please :)
 
 int main() {
     _Log_("Hello from main!");
--- include/dsl_maker/macros/RunFunction.h
+++ include/dsl_maker/macros/RunFunction.h
@@ -14,6 +14,18 @@
 
 #define _DSL_MAKER_NEW_FUNCTION_NAME(count) _DSL_MAKER_NEW_FUNCTION_NAME_FROM_COUNT(__COUNTER__)
 
+#define _DSL_MAKER_REGISTER_NEW_FUNCTION(lambdaRegistrationFunction, count)                  \
+    inline void _DSL_MAKER_NEW_FUNCTION_NAME_FROM_COUNT(count)();                            \
+    _DSL_MAKER_NEW_FUNCTION_RUNNER_VARIABLE_NAME_FROM_COUNT(count                            \
+    ){[]() { lambdaRegistrationFunction(_DSL_MAKER_NEW_FUNCTION_NAME_FROM_COUNT(count)); }}; \
+    inline void _DSL_MAKER_NEW_FUNCTION_NAME_FROM_COUNT(count)
+
+#define _DSL_MAKER_REGISTER_NEW_FUNCTION_WITH_ARGUMENTS(lambdaRegistrationFunction, count, ...) \
+    inline void _DSL_MAKER_NEW_FUNCTION_NAME_FROM_COUNT(count)(__VA_ARGS__);                    \
+    _DSL_MAKER_NEW_FUNCTION_RUNNER_VARIABLE_NAME_FROM_COUNT(count                               \
+    ){[]() { lambdaRegistrationFunction(_DSL_MAKER_NEW_FUNCTION_NAME_FROM_COUNT(count)); }};    \
+    inline void _DSL_MAKER_NEW_FUNCTION_NAME_FROM_COUNT(count)
+
 #define _DSL_MAKER_REGISTER_NEW_FUNCTION_LAMBDA(lambdaRegistrationFunction, count)              \
     inline void _DSL_MAKER_NEW_FUNCTION_NAME_FROM_COUNT(count)();                               \
     _DSL_MAKER_NEW_FUNCTION_RUNNER_VARIABLE_NAME_FROM_COUNT(count){[]() {                       \
