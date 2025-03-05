f7ad059e0a79e9e6cbda8999f01e0db33d2c26eb
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-04 18:56:02 -0800

    Add missing anonymous namespace

--- include/global_macro_functions.h
+++ include/global_macro_functions.h
@@ -132,16 +132,19 @@ namespace _GlobalMacroFunctions {
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-#define __GLOBAL_MACRO_FUNCTIONS_REGISTER_NEW_NAMED_FUNCTION_BODY(                                \
-    symbol, registrationFunction, count                                                           \
-)                                                                                                 \
-    inline void _GLOBAL_MACRO_FUNCTIONS_NEW_NAMED_FUNCTION_NAME_FROM_COUNT(symbol, count)();      \
-    _GLOBAL_MACRO_FUNCTIONS_NEW_NAMED_FUNCTION_RUNNER_VARIABLE_NAME_FROM_COUNT(symbol, count){[]( \
-                                                                                              ) { \
-        registrationFunction(                                                                     \
-            _GLOBAL_MACRO_FUNCTIONS_NEW_NAMED_FUNCTION_NAME_FROM_COUNT(symbol, count)             \
-        );                                                                                        \
-    }};                                                                                           \
+#define __GLOBAL_MACRO_FUNCTIONS_REGISTER_NEW_NAMED_FUNCTION_BODY(                                 \
+    symbol, registrationFunction, count                                                            \
+)                                                                                                  \
+    inline void _GLOBAL_MACRO_FUNCTIONS_NEW_NAMED_FUNCTION_NAME_FROM_COUNT(symbol, count)();       \
+    namespace {                                                                                    \
+        _GLOBAL_MACRO_FUNCTIONS_NEW_NAMED_FUNCTION_RUNNER_VARIABLE_NAME_FROM_COUNT(symbol, count){ \
+            []() {                                                                                 \
+                registrationFunction(                                                              \
+                    _GLOBAL_MACRO_FUNCTIONS_NEW_NAMED_FUNCTION_NAME_FROM_COUNT(symbol, count)      \
+                );                                                                                 \
+            }                                                                                      \
+        };                                                                                         \
+    }                                                                                              \
     inline void _GLOBAL_MACRO_FUNCTIONS_NEW_NAMED_FUNCTION_NAME_FROM_COUNT(symbol, count)
 
 #define _GLOBAL_MACRO_FUNCTIONS_REGISTER_NEW_NAMED_FUNCTION(symbol, registrationFunction) \
